1212bc809fc20c515545d2ecec4449b6e45e4af1
Brian Joe Kamau <joebrian998@gmail.com>
2024-09-30 10:39:18 +0300

    updated packages

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(pickUpLocation) {
+    const hqLocation = 42;
+    return Math.abs(pickUpLocation - hqLocation); // Returns the absolute distance in blocks
+}
+
+function distanceFromHqInFeet(pickUpLocation) {
+    const blocks = distanceFromHqInBlocks(pickUpLocation);
+    return blocks * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const blockTravelled = Math.abs(destination - start); //calculating blocks distance
+    return blockTravelled * 264;
+}
+function calculatesFarePrice(start,destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0;
+     }
+     else if(distance >400 && distance <=2000) {
+        return (distance - 400) * 0.02;
+    }
+    else if(distance >=2000 && distance <2500) {
+        return 25;
+    }
+    else{
+        return "cannot travel that far"
+    }
+}
